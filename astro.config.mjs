40d93918e1ac999932a9e8ae097ea7051493dc85
albert-queralto <albert.queralto.lopez@gmail.com>
2026-05-29 23:20:23 +0200

    feat: add KaTeX support for math rendering and update layout for head slot

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   vite: {
     resolve: {
       alias: {
